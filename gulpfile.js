94ace83e25a58684e2d38af5a2d450e3f6bfdc85
Илья Тюрин <il08082000@gmail.com>
2019-12-03 07:03:45 +0700

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,35 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const browserSync = require("browser-sync").create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
+function minify() {
+  gulp.task('default', function() {
+    gulp
+      .src('src/**/*.css')
+      .pipe(cssmin())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest('dist'));
+  });
+}
 
 function style() {
   return gulp
-    .src("./scss/**/*.scss")
+    .src('./scss/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest("./css/"))
+    .pipe(gulp.dest('./css/'))
     .pipe(browserSync.stream());
 }
 
 function watch() {
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: './'
     }
   });
-  gulp.watch("./scss/**/*.scss", style);
-  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./*.html').on('change', browserSync.reload);
 }
 exports.style = style;
 exports.watch = watch;
